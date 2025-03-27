1dfcfd1eac5738f23156eb4fd2e0d0f7350addb9
nikolas-cyclone <n.patsalidis@cyi.ac.cy>
2025-03-27 17:29:45 +0200

    potential evolution plot updated

--- main_scripts/potential_evolution.py
+++ main_scripts/potential_evolution.py
@@ -26,8 +26,15 @@ def label_nicely(s,last=False):
         return r'$\alpha$'
     else:
         return f'${s}$'
+def title(key):
+    title = f' {key[0]} interactions of type {key[1]}'
+    return title
 def ylabel(key):
-    ylabel = f'{key[-1]} parameters for {key[0]} interactions of type {key[1]}'
+    if key[-1] =='MorseBond':
+        a = 'Morse(Bond)'
+    else:
+        a = key[-1]
+    ylabel = f'{a} parameter values'
     return ylabel
 def main():
     
@@ -58,25 +65,32 @@ def main():
                 v = model.pinfo[k].value
                 print(ty,pn,v)
                 model_parameters[ty][pn].append( v )
-    print(model_parameters)
     iters = np.arange(1,args.maxnum+2,1,dtype=int)
     size = 3.3
     _ = plt.figure(figsize=(size, size),dpi=600)
-    plt.minorticks_on()
-    plt.tick_params(direction='in', which='minor',length = size)
-    plt.tick_params(direction='in', which='major',length = 2*size)
     ax = plt.gca()
 
     colors = ['#b2182b','#d6604d','#f4a582','#fddbc7','#d1e5f0','#92c5de','#4393c3','#2166ac']*10
     markers = ['s','o','*','v','^']*10
     ls = ['-','--','-.',':']*10
     naxis = len(model_parameters)
-    cols = math.ceil(math.sqrt(naxis))
-    rows = math.ceil(naxis / cols)
+    rows = math.ceil(math.sqrt(naxis))
+    cols = math.ceil(naxis / rows)
 
-    # Create subplots
+    sorted_model_params = dict()
+    helping_dir = {'BO':['MorseBond','Bezier'],'AN':['harmonic'], 'PW':['Morse','Bezier'], 'LD': ['Bezier'] }
+    for c in ['BO','AN','PW','LD']:
+        for k in helping_dir[c]:
+            for key, params in model_parameters.items():
+                if key[0] == c  and k == key[-1]:
+                    sorted_model_params[key] = params
+                    print(key)
+            # Create subplots
     fig, axes = plt.subplots(rows, cols, figsize=(cols * 4, rows * 4))
 
+    plt.minorticks_on()
+    plt.tick_params(direction='in', which='minor',length = size)
+    plt.tick_params(direction='in', which='major',length = 2*size)
     # Flatten axes for easy iteration (in case of 2D array)
     axes = axes.flatten()
 
@@ -84,12 +98,12 @@ def main():
     e= 6 if len(iters) >60 else 4
     if len(iters)<30: e = 2
     xticks = [int(x) for x in iters if (x)%e == 0]
-    for i,(key, params) in enumerate(model_parameters.items()):
+    for i,(key, params) in enumerate(sorted_model_params.items()):
         ax = axes[i]
         ax.xaxis.set_minor_locator(ticker.FixedLocator(odd_numbers))
-        ax.set_title(ylabel(key), fontsize=2.3*size)
-        ax.set_ylabel('Parameter Values',fontsize=2.8*size)
-        ax.set_xlabel('AL iteration',fontsize=2.8*size)
+        ax.set_title(title(key), fontsize=3.3*size)
+        ax.set_ylabel(ylabel(key),fontsize=3.3*size)
+        ax.set_xlabel('AL iteration',fontsize=3.3*size)
         ax.grid(True,alpha=0.35)
         ax.set_xticks(xticks)
         mx,mn = -1e16,1e16
@@ -103,9 +117,9 @@ def main():
             last = j+1 == len(params)
             ax.plot(iters, v,label = label_nicely(pn, last), color= colors[j],lw=0.2*size,marker=markers[j] )
         r = mx - mn
-        ax.set_ylim(mn-0.1*r,mx+0.35*r)
+        ax.set_ylim(mn-0.1*r,mx+0.45*r)
         #ax.set_yscale('log')
-        ax.legend(frameon=False,ncol=int(len(params)/3)+1,fontsize=2.1*size)
+        ax.legend(frameon=False,ncol=int(len(params)/3)+1,fontsize=2.5*size)
     for i in range(naxis, len(axes)):
         fig.delaxes(axes[i])
 
